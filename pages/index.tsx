cd1fde642f129f98025314f471507dfad0aaf58d
abdelmomen1985 <abdelmomen1985@gmail.com>
2021-06-08 18:20:00 +0000

    tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
 
-<div className="flex flex-col w-full">
-	<div className="p-1  mt-2 text-center space-x-1 space-y-2">
-		<button className="bg-gray-50 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">Gray50</button>
-	</div>
+    <div className="flex flex-col w-full">
+      <div className="p-1  mt-2 text-center space-x-1 space-y-2">
+        <button className="bg-gray-50 px-5 py-3 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+          Welcome tailwind!
+        </button>
+      </div>
     </div>
-
 
     <p>
       <Link href="/about">
@@ -18,6 +19,6 @@ const IndexPage = () => (
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;

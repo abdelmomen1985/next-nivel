049f2bf417c2a861f37f6d7fbbe4bef7ee7ec3f5
Momen <abdelmomen1985@gmail.com>
2021-06-13 19:06:18 +0200

    context

--- pages/[lang]/meetings/index.tsx
+++ pages/[lang]/meetings/index.tsx
@@ -1,17 +1,13 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
-import React from 'react'
-import Layout from '../../../Layouts/Layout';
-import { getLocalizationProps } from '../../../Context/LangContext';
+import { GetStaticPaths, GetStaticProps } from "next";
+import React from "react";
+import Layout from "../../../Layouts/Layout";
+import { getLocalizationProps } from "../../../context/LangContext";
 
 const MeetingsPage = () => {
-  return (
-    <Layout>
-      Rooms page
-    </Layout>
-  )
-}
+  return <Layout>Rooms page</Layout>;
+};
 
-export default MeetingsPage
+export default MeetingsPage;
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const localization = getLocalizationProps(ctx, "common");

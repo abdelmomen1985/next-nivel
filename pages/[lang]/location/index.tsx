24015fc53f02bbfd96e1325d512ef03b4bc80477
Momen <abdelmomen1985@gmail.com>
2021-06-27 19:40:06 +0200

    strapi is working

--- pages/[lang]/location/index.tsx
+++ pages/[lang]/location/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
 import { getLocalizationProps } from "../../../context/LangContext";
+import Layout from "../../../Layouts/Layout";
+import { LayoutType } from "../../../types/layout";
 
 const LocationPage = () => {
-  return <Layout>Rooms page</Layout>;
+  return <Layout layout={{} as LayoutType}>Rooms page</Layout>;
 };
 
 export default LocationPage;
--- query/meeting_rooms.ts
+++ query/meeting_rooms.ts
@@ -1,19 +1,17 @@
-import { gql } from '@apollo/client';
-import { LAYOUT_FRAGMENT } from './fragments/layout';
-
+import { gql } from "@apollo/client";
+import { LAYOUT_FRAGMENT } from "./fragments/layout";
 
 export const MEETING_ROOMS = gql`
-  {LAYOUT_FRAGMENT}
+  ${LAYOUT_FRAGMENT}
   query meeting_rooms {
-    meeting_rooms(order_by: {created_at: asc})
-    {
+    meeting_rooms(order_by: { created_at: asc }) {
       guests
       id
       space
       title
-   }
-   layout{
-     ...LayoutFragment
-   }
+    }
+    layout {
+      ...LayoutFragment
+    }
   }
-`
+`;

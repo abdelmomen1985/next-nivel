b68f7ea1cdfd4b11749f1eb84941defa81f40848
abdelmomen1985 <abdelmomen1985@gmail.com>
2024-12-05 15:15:33 +0200

    working now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
+import { ApolloProvider } from "@apollo/client";
 import "rc-steps/assets/index.css";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import "react-slideshow-image/dist/styles.css";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AppContextProvider from "../context/AppContext";
 // import { AppProps } from 'next/app'
+import { LanguageProvider } from "../context/LangContext";
 import { useApollo } from "../lib/apolloClient";
 import "../styles/index.css";
 
 function MyApp(ctx: any) {
   const apolloClient = useApollo(ctx.pageProps.initialApolloState);
   return (
-    /*
     <ApolloProvider client={apolloClient}>
       <AppContextProvider>
         <LanguageProvider localization={ctx.pageProps.localization}>
@@ -28,8 +31,6 @@ function MyApp(ctx: any) {
         </LanguageProvider>
       </AppContextProvider>
     </ApolloProvider>
-    */
-    <div>Under Maintenance </div>
   );
 }
 

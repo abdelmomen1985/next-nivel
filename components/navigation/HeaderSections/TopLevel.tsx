ed749d2c9ecf15a47d56b66dc184b24851838c2c
abdelmomen1985 <abdelmomen1985@gmail.com>
2025-02-04 11:02:26 +0200

    fixed

--- components/navigation/HeaderSections/TopLevel.tsx
+++ components/navigation/HeaderSections/TopLevel.tsx
@@ -1,16 +1,21 @@
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import clsx from "clsx";
-import Link from "next/link";
-import router from "next/router";
-import { useContext } from "react";
-import { LayoutType } from "../../../types/layout";
-import styles from "../navigation.module.scss";
-import { AppContext } from "./../../../context/AppContext";
-import { useSignout } from "./../../../hooks/useSignout";
-import useTranslation from "./../../../hooks/useTranslation";
-import ActiveLink from "./../ActiveLink";
-import LocaleSwitch from "./LocaleSwitch";
+import {
+  faBars,
+  faMinusCircle,
+  faPlusCircle,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import clsx from 'clsx'
+import Link from 'next/link'
+import router from 'next/router'
+import { useContext } from 'react'
+import { LayoutType } from '../../../types/layout'
+import styles from '../navigation.module.scss'
+import { AppContext } from './../../../context/AppContext'
+import { useSignout } from './../../../hooks/useSignout'
+import useTranslation from './../../../hooks/useTranslation'
+import ActiveLink from './../ActiveLink'
+import LocaleSwitch from './LocaleSwitch'
 
 const TopLevel = ({
   setOpenNav,
@@ -18,29 +23,29 @@ const TopLevel = ({
   layout,
   setIsRegister,
 }: {
-  setOpenNav: (open: boolean) => void;
-  openNav: boolean;
-  layout: LayoutType;
-  setIsRegister: (isReg: boolean) => void;
+  setOpenNav: (open: boolean) => void
+  openNav: boolean
+  layout: LayoutType
+  setIsRegister: (isReg: boolean) => void
 }) => {
-  const { locale, t } = useTranslation();
-  const { isMobile, isTablet, setLoginModal, user } = useContext(AppContext);
+  const { locale, t } = useTranslation()
+  const { isMobile, isTablet, setLoginModal, user } = useContext(AppContext)
 
-  const { signOutHandler } = useSignout();
+  const { signOutHandler } = useSignout()
   const navToProfileHandler = () => {
     if (user) {
-      router.push(`/${locale}/profile`);
+      router.push(`/${locale}/profile`)
     } else {
-      setLoginModal(true);
+      setLoginModal(true)
     }
-  };
+  }
   return (
     <div className="text-center flex-wrap justify-between lg:justify-between items-center my-2 mx-2 md:mx-5 px-1 md:px-2">
       <Link href={`/${locale}/`}>
         <img
           src={layout?.remoteSchemaUrl + layout?.top_logo_en?.url}
           className="cursor-pointer mx-auto"
-          style={{ height: "80px" }}
+          style={{ height: '80px' }}
         />
       </Link>
       <div className="flex justify-center items-center">
@@ -49,14 +54,14 @@ const TopLevel = ({
             <div
               className={clsx(
                 styles.userMenu,
-                isMobile ? "hidden" : "hidden md:flex"
+                isMobile ? 'hidden' : 'hidden md:flex',
               )}
             >
               <LocaleSwitch />
               {isMobile ? (
                 <hr
                   className="mx-auto my-2 w-1/2 bg-primary-dark border-transparent"
-                  style={{ height: "2px" }}
+                  style={{ height: '2px' }}
                 />
               ) : (
                 <span>{/*|*/}</span>
@@ -68,8 +73,8 @@ const TopLevel = ({
                 >
                   <a
                     className={clsx(
-                      isMobile || isTablet ? "my-2" : "my-0",
-                      "mx-3 font-medium text-primary-dark"
+                      isMobile || isTablet ? 'my-2' : 'my-0',
+                      'mx-3 font-medium text-primary-dark',
                     )}
                   >
                     {user?.name}
@@ -79,7 +84,7 @@ const TopLevel = ({
               {isMobile ? (
                 <hr
                   className="mx-auto my-2 w-1/2 bg-primary-dark border-transparent"
-                  style={{ height: "2px" }}
+                  style={{ height: '2px' }}
                 />
               ) : (
                 <span>|</span>
@@ -88,24 +93,24 @@ const TopLevel = ({
                 <button
                   onClick={signOutHandler}
                   className={clsx(
-                    isMobile || isTablet ? "my-2" : "my-0",
-                    "mx-3 font-medium text-primary-dark"
+                    isMobile || isTablet ? 'my-2' : 'my-0',
+                    'mx-3 font-medium text-primary-dark',
                   )}
                 >
-                  {t("signOut")}
+                  {t('signOut')}
                 </button>
               ) : (
                 <button
                   onClick={() => {
-                    setIsRegister(false);
-                    setLoginModal(true);
+                    setIsRegister(false)
+                    setLoginModal(true)
                   }}
                   className={clsx(
-                    isMobile || isTablet ? "my-2" : "my-0",
-                    "mx-3 font-medium text-primary-dark"
+                    isMobile || isTablet ? 'my-2' : 'my-0',
+                    'mx-3 font-medium text-primary-dark',
                   )}
                 >
-                  {t("signIn")}
+                  {t('signIn')}
                 </button>
               )}
 
@@ -113,7 +118,7 @@ const TopLevel = ({
                 <>
                   <hr
                     className="mx-auto my-2 w-1/2 bg-primary-dark border-transparent"
-                    style={{ height: "2px" }}
+                    style={{ height: '2px' }}
                   />
                 </>
               )}
@@ -127,9 +132,9 @@ const TopLevel = ({
               <img
                 src={user?.media?.profile_img?.secure_url}
                 style={{
-                  width: "50px",
-                  height: "50px",
-                  borderRadius: "50%",
+                  width: '50px',
+                  height: '50px',
+                  borderRadius: '50%',
                 }}
               />
             ) : (
@@ -143,32 +148,32 @@ const TopLevel = ({
           </button>
           <span>|</span>
           <div className="mx-3 flex">
-            {/* <FontAwesomeIcon
+            <FontAwesomeIcon
               icon={faPlusCircle}
               className="text-primary-light text-2xl "
               onClick={() => {
                 const currentZoom = (
-                  (document.body.style as any)["zoom"] + ""
-                ).replace("%", "");
-                const currentZoomLevel = +currentZoom || 100;
+                  (document.body.style as any)['zoom'] + ''
+                ).replace('%', '')
+                const currentZoomLevel = +currentZoom || 100
 
-                (document.body.style as any)["zoom"] =
-                  currentZoomLevel + 10 + "%";
+                ;(document.body.style as any)['zoom'] =
+                  currentZoomLevel + 10 + '%'
               }}
             />
             <FontAwesomeIcon
               icon={faMinusCircle}
               onClick={() => {
                 const currentZoom = (
-                  (document.body.style as any)["zoom"] + ""
-                ).replace("%", "");
-                const currentZoomLevel = +currentZoom || 100;
+                  (document.body.style as any)['zoom'] + ''
+                ).replace('%', '')
+                const currentZoomLevel = +currentZoom || 100
 
-                (document.body.style as any)["zoom"] =
-                  currentZoomLevel - 10 + "%";
+                ;(document.body.style as any)['zoom'] =
+                  currentZoomLevel - 10 + '%'
               }}
               className="text-primary-light text-2xl "
-            /> */}
+            />
           </div>
         </div>
         {isMobile && (
@@ -176,14 +181,14 @@ const TopLevel = ({
             icon={openNav ? faTimes : faBars}
             className="text-primary-dark text-3xl mx-1 mt-1 cursor-pointer"
             onClick={(e: any) => {
-              e.stopPropagation();
-              setOpenNav((prev: boolean) => !prev);
+              e.stopPropagation()
+              setOpenNav((prev: boolean) => !prev)
             }}
           />
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TopLevel;
+export default TopLevel
--- hooks/useSpeech.ts
+++ hooks/useSpeech.ts
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
-import { useSpeechSynthesis } from "react-speech-kit";
-import useTranslation from "./useTranslation";
+import { useEffect, useState } from 'react'
+import { useSpeechSynthesis } from 'react-speech-kit'
+import useTranslation from './useTranslation'
 export function useSpeech() {
-  const { locale } = useTranslation();
-  const { speak, voices, cancel } = useSpeechSynthesis();
+  const { locale } = useTranslation()
+  const { speak, voices, cancel } = useSpeechSynthesis()
   const arIndex =
     (voices as SpeechSynthesisVoice[])?.findIndex((item) =>
-      item.lang.includes("ar")
-    ) || 1;
+      item.lang.includes('ar'),
+    ) || 1
   const enIndex =
     (voices as SpeechSynthesisVoice[])?.findIndex((item) =>
-      item.lang.includes("en")
-    ) || 1;
+      item.lang.includes('en'),
+    ) || 1
 
   const [currentVoiceIndex, setCurrentVoiceIndex] = useState<number>(() =>
-    locale === "ar" ? arIndex : enIndex
-  );
+    locale === 'ar' ? arIndex : enIndex,
+  )
 
   useEffect(() => {
-    if (locale === "ar") {
-      setCurrentVoiceIndex(arIndex);
+    if (locale === 'ar') {
+      setCurrentVoiceIndex(arIndex)
     } else {
-      setCurrentVoiceIndex(enIndex);
+      setCurrentVoiceIndex(enIndex)
     }
-  }, [locale, arIndex, enIndex]);
+  }, [locale, arIndex, enIndex])
 
   const speechHandler = (text: string) => {
-    cancel();
-    console.log("text", text);
-    console.log("currentVoiceIndex", currentVoiceIndex);
-    speak({ text, voice: voices[currentVoiceIndex] });
+    cancel()
+    console.log('text', text)
+    console.log('currentVoiceIndex', currentVoiceIndex)
+    speak({ text, voice: voices[currentVoiceIndex] })
 
-    // var synth = window.speechSynthesis;
-    // synth.cancel();
-    // var msg = new SpeechSynthesisUtterance();
-    // msg.lang = locale;
-    // msg.text = text;
-    // window.speechSynthesis.speak(msg);
-  };
-  return { speechHandler };
+    var synth = window.speechSynthesis
+    synth.cancel()
+    var msg = new SpeechSynthesisUtterance()
+    msg.lang = locale
+    msg.text = text
+    window.speechSynthesis.speak(msg)
+  }
+  return { speechHandler }
 }
